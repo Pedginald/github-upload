f2d160e1361ef5c48d65a937442bdaa24b474867
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-16 09:46:24 -0400

    Adding some comments

--- script.js
+++ script.js
@@ -3,43 +3,49 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var h1 = document.querySelector("h1");
 
+//Add class to h1 tag
 h1.classList.add("coolTitle");
 
 function inputLength() {
 	return input.value.length;
 }
 
+//Add div containing a list item and aremove button
 function createListElement() {
 	var div = document.createElement("div");
 	div.classList.add("itemRow");
 	var li = document.createElement("li");
 	var button = document.createElement("button");
-	ul.appendChild(div);
-	div.append(li, button);
+	button.classList.add("remove");
 	button.appendChild(document.createTextNode("Remove"));
 	li.appendChild(document.createTextNode(input.value));
-	button.classList.add("remove");
+	div.append(li, button);
+	ul.appendChild(div);
 	input.value = "";
 }
 
+//Creates list item and button after mouse click
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
+//Create list item and button after press of return key
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
 
+//Delete item when remove button is clicked
 function deleteItem(item) {
 	if(item.target.className === "remove") {
 		item.target.parentElement.remove();
 	}
 };
 
+//Toggle strikethrough (done class) when item is clicked
 function taskComplete(item) {
 	if (item.target.tagName === "LI")
 	item.target.classList.toggle("done");
