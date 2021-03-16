5b84067fd731bebf82ea6a2d773eebb117237e38
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-03-16 11:51:14 -0400

    Cleaning up some JavaScript formatting

--- script.js
+++ script.js
@@ -7,49 +7,49 @@ var h1 = document.querySelector("h1");
 h1.classList.add("coolTitle");
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 //Add div containing a list item and aremove button
 function createListElement() {
-	var div = document.createElement("div");
-	div.classList.add("itemRow");
-	var li = document.createElement("li");
-	var button = document.createElement("button");
-	button.classList.add("remove");
-	button.appendChild(document.createTextNode("Remove"));
-	li.appendChild(document.createTextNode(input.value));
-	div.append(li, button);
-	ul.appendChild(div);
-	input.value = "";
+    var div = document.createElement("div");
+    div.classList.add("itemRow");
+    var li = document.createElement("li");
+    var button = document.createElement("button");
+    button.classList.add("remove");
+    button.appendChild(document.createTextNode("Remove"));
+    li.appendChild(document.createTextNode(input.value));
+    div.append(li, button);
+    ul.appendChild(div);
+    input.value = "";
 }
 
 //Creates list item and button after mouse click
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+    if (inputLength() > 0) {
+        createListElement();
+    }
 }
 
 //Create list item and button after press of return key
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+    if (inputLength() > 0 && event.keyCode === 13) {
+      createListElement();
+    }
 }
 
 //Delete item when remove button is clicked
 function deleteItem(item) {
-	if(item.target.className === "remove") {
-		item.target.parentElement.remove();
-	}
-};
+    if(item.target.className === "remove") {
+        item.target.parentElement.remove();
+    }
+}
 
 //Toggle strikethrough (done class) when item is clicked
 function taskComplete(item) {
-	if (item.target.tagName === "LI")
-	item.target.classList.toggle("done");
-};
+    if (item.target.tagName === "LI")
+        item.target.classList.toggle("done");
+}
 
 button.addEventListener("click", addListAfterClick);
 
